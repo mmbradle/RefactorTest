57aaef63790efeb19261d40687e1254d8cb08b83
mmbradle <mmbradle@gmail.com>
2013-03-14 12:00:38 -0500

    Started refactor of statement()

--- src/Customer.java
+++ src/Customer.java
@@ -18,48 +18,30 @@ class Customer {
     }
 
     public String getStatement() {
-        
+
         double totalPrice = 0;
         int frequentRenterPoints = 0;
         StringBuilder stringBuilder = new StringBuilder("Rental Record for");
         stringBuilder.append(getName());
         stringBuilder.append("\n");
-        for (Rental each: this.rentals) {
-            double thisAmount = 0;
-            // determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-                default:
-                    throw new IllegalArgumentException(each.getMovie().getPriceCode() + " is not a valid priceCode");
-            }
+        for (Rental each : this.rentals) {
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints++;
             // show figures for this rental
-            stringBuilder.append("\t"); 
+            stringBuilder.append("\t");
             stringBuilder.append(each.getMovie().getTitle());
             stringBuilder.append("\t");
-            stringBuilder.append(thisAmount);
+            stringBuilder.append(each.getCharge());
             stringBuilder.append("\n");
-            totalPrice += thisAmount;
+            totalPrice += each.getCharge();
         }
         // add footer lines
         stringBuilder.append("Amount owed is " + String.valueOf(totalPrice) + "\n");
         stringBuilder.append("You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points");
         return stringBuilder.toString();
     }
+
 }
--- src/Rental.java
+++ src/Rental.java
@@ -15,4 +15,27 @@ class Rental {
     public Movie getMovie() {
         return _movie;
     }
+    
+    public double getCharge() {
+        double charge = 0;
+        // determine amounts for each line
+        switch (this.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                charge += 2;
+                if (this.getDaysRented() > 2)
+                    charge += (this.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                charge += this.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                charge += 1.5;
+                if (this.getDaysRented() > 3)
+                    charge += (this.getDaysRented() - 3) * 1.5;
+                break;
+            default:
+                throw new IllegalArgumentException(this.getMovie().getPriceCode() + " is not a valid priceCode");
+        }
+        return charge;
+    }
 }
